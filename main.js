9191880d0d2d3f891ccc8e492c8a15e1f204f7f7
nataaffonso <106812814+nataaffonso@users.noreply.github.com>
2024-04-15 09:11:49 -0300

    GGAD

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function verificarTime() {
     var respostaTime = prompt("Qual o melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "CORINTHAIANS pr") {
+      alert("Isso mesmo! O CORINTHANS PR é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
