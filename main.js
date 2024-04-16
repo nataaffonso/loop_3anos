816cead4c45c051736a731c4a53aa0ee94a5e7e7
nataaffonso <106812814+nataaffonso@users.noreply.github.com>
2024-04-16 07:51:49 -0300

    brasil

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("copa.do mundo");
 
     
     if (respostaTime.toLowerCase() === "CORINTHAIANS pr") {
